6f4b9ef1749a4efe5c677fe92874971c60a67ba1
サジッド(sazid) <mdip@uxd.co.jp>
2022-08-04 15:23:56 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by UXD on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

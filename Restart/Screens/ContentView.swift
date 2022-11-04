50b3e4f13955bc2515368bdb5e6b867253a979d3
サジッド(sazid) <mdip@uxd.co.jp>
2022-11-04 11:08:03 +0900

    second commit

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by UXD on 2022/08/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack{
+            if isOnboardingViewActive{
+                OnboardingView()
+            }else{
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13")
+    }
+}
---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,82 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by UXD on 2022/08/04.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    //Mark:Property
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool=false
+    //Mark:Body
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            //Mark:Header
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y:isAnimating ? 35 : -35)
+                    .animation(
+                        Animation
+                            .easeOut(duration: 4)
+                            .repeatForever()
+                        ,   value:isAnimating
+                    )
+            }
+            
+            //Mark:Center
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            //Mark:Footer
+            
+            Spacer()
+            
+            Button(action: {
+                withAnimation{
+                    playSound(sound: "success", type: "m4a")
+                    isOnboardingViewActive=true
+                }
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//Button
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+        }//:VSTACK
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline:.now()+0.5,execute:{
+                isAnimating=true
+            })
+        })
+    }
+}
+//Mark:Preview
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
